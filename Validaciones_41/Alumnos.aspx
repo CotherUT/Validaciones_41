0bfa922c026a8900d2b53a68ecfe145aad86d75b
CotherUT <tic-290025@utnay.edu.mx>
2022-09-26 11:06:55 -0600

    Agregar archivos de proyecto.

---
+++ Validaciones_41/Alumnos.aspx
@@ -0,0 +1,85 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Alumnos.aspx.cs" Inherits="Validaciones_41.Alumnos" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table>
+            <tr>
+                <td>
+                    <asp:Label ID="lbNombre" runat="server" Text="Nombre:" ></asp:Label>
+                 </td>
+                <td>
+                    <asp:TextBox ID="tbNombre" runat="server" Width="340"></asp:TextBox>
+                </td>
+                <td></td>
+                <td>
+                    <asp:Label ID="lbDomicilio" runat="server" Text="Domicilio:"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="tbDomicilio" runat="server" Width="340"></asp:TextBox>
+                </td>
+                <td></td>
+            </tr>
+             <tr>
+                <td>
+                    <asp:Label ID="lbTelefono" runat="server" Text="Telefono:" ></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="tbTelefono" runat="server" Width="340"></asp:TextBox>
+                </td>
+                <td></td>
+                <td>
+                     <asp:Label ID="sexo" runat="server" Text="Sexo:" ></asp:Label>
+                </td>
+                <td>
+                    <asp:DropDownList ID="ddlSExo" runat="server" Width="340">
+                        <asp:ListItem></asp:ListItem>
+                        <asp:ListItem>Femenino</asp:ListItem>
+                        <asp:ListItem>Masculino</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+                <td></td>
+            </tr>
+             <tr>
+                <td>
+                    <asp:Label ID="lbEmail" runat="server" Text="Email:" ></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="tbEmail" runat="server" Width="340"></asp:TextBox>
+                </td>
+                <td></td>
+                <td>
+                    <asp:Label ID="lbConfirmaEmail" runat="server" Text="Confirmar Email:" ></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="tbConfirmarEmail" runat="server" Width="340"></asp:TextBox>
+                </td>
+                <td></td>
+            </tr>
+             <tr>
+                <td>
+                    <asp:Label ID="lbEdad" runat="server" Text="Edad:" ></asp:Label>
+                </td>
+                <td>
+                     <asp:TextBox ID="tbEdad" runat="server" Width="340"></asp:TextBox>
+                </td>
+                <td></td>
+                <td></td>
+                <td>
+                    <asp:Button ID="btGuardar" runat="server" Text="Guardar" />
+                </td>
+                <td></td>
+            </tr>
+
+        </table>
+      
+        
+    </form>
+</body>
+</html>
---
+++ Validaciones_41/Alumnos.aspx.designer.cs
@@ -0,0 +1,161 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Validaciones_41
+{
+
+
+    public partial class Alumnos
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control lbNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbNombre;
+
+        /// <summary>
+        /// Control tbNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbNombre;
+
+        /// <summary>
+        /// Control lbDomicilio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbDomicilio;
+
+        /// <summary>
+        /// Control tbDomicilio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbDomicilio;
+
+        /// <summary>
+        /// Control lbTelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbTelefono;
+
+        /// <summary>
+        /// Control tbTelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbTelefono;
+
+        /// <summary>
+        /// Control sexo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label sexo;
+
+        /// <summary>
+        /// Control ddlSExo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlSExo;
+
+        /// <summary>
+        /// Control lbEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbEmail;
+
+        /// <summary>
+        /// Control tbEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbEmail;
+
+        /// <summary>
+        /// Control lbConfirmaEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbConfirmaEmail;
+
+        /// <summary>
+        /// Control tbConfirmarEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbConfirmarEmail;
+
+        /// <summary>
+        /// Control lbEdad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbEdad;
+
+        /// <summary>
+        /// Control tbEdad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbEdad;
+
+        /// <summary>
+        /// Control btGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btGuardar;
+    }
+}
